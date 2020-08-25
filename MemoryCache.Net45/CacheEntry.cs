31eb4fe38fec3b9ca930310b03881ad92e91a216
Timothy D Meadows II <timothy@timothymeadows.com>
2020-08-25 11:24:51 -0700

    Add MemoryCache.Net45

---
+++ MemoryCache.Net45/CacheEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MemoryCache.Net
+{
+    [StructLayout(LayoutKind.Sequential)]
+    [Serializable]
+    public struct CacheEntry<T>
+    {
+        public bool Serializable;
+        public string Key;
+        public T Value;
+    }
+}
